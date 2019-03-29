5747d00dc0120cfccafa36dba1f2ae8a3b4a64da
brandon corey <bcorey85@gmail.com>
2019-03-29 03:22:31 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,49 +1,55 @@
-var chocolateBars = [
-  'snickers',
-  'hundred grand',
-  'kitkat',
-  'skittles'
-]
+// var chocolateBars = [
+//   'snickers',
+//   'hundred grand',
+//   'kitkat',
+//   'skittles'
+// ]
 
-function addElementToBeginningOfArray(array, element){
-    return([element,...array])
-}
+// function addElementToBeginningOfArray(array, element){
+//     return([element,...array])
+// }
 
-function destructivelyAddElementToBeginningOfArray(array, element) {
-  array.unshift(element)
-  return(array)
-}
+// function destructivelyAddElementToBeginningOfArray(array, element) {
+//   array.unshift(element)
+//   return(array)
+// }
 
-function addElementToEndOfArray(array, element) {
-    return([...array, element])
+// function addElementToEndOfArray(array, element) {
+//     return([...array, element])
 
-}
+// }
 
-function destructivelyAddElementToEndOfArray(array, element) {
-  array.push(element)
-  return (array)
-}
+// function destructivelyAddElementToEndOfArray(array, element) {
+//   array.push(element)
+//   return (array)
+// }
 
-function accessElementInArray(array, index) {
-  return(array[index])
-}
+// function accessElementInArray(array, index) {
+//   return(array[index])
+// }
 
-function  removeElementFromBeginningOfArray(array) {
+// function  removeElementFromBeginningOfArray(array) {
 
- return(array.slice(1))
+// return(array.slice(1))
 
-}
+// }
 
-function destructivelyRemoveElementFromBeginningOfArray(array) {
-  array.shift(1)
-  return(array)
-}
+// function destructivelyRemoveElementFromBeginningOfArray(array) {
+//   array.shift(1)
+//   return(array)
+// }
 
-function removeElementFromEndOfArray(array) {
-  return(array.slice(0, array.length-1))
-}
+// function removeElementFromEndOfArray(array) {
+//   return(array.slice(0, array.length-1))
+// }
 
-function destructivelyRemoveElementFromEndOfArray(array) {
-  array.pop(1)
-  return(array)
-}
+// function destructivelyRemoveElementFromEndOfArray(array) {
+//   array.pop(1)
+//   return(array)
+// }
+
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+
+
+
