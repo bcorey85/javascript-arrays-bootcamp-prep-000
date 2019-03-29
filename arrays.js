5edde6e6fa2318b93b4cbd52db67c7756ce7d5de
brandon corey <bcorey85@gmail.com>
2019-03-29 03:30:36 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -61,8 +61,7 @@ function destructivelyAddElementToBeginningOfArray(array,element){
 }
 
 function addElementToEndOfArray(array, element){
-  array.push(element)
-  return array
+ array = [array, ...element]
 }
 
 function destructivelyAddElementToEndOfArray(array, element){
